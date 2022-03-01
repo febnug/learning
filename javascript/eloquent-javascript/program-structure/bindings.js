ebae2f307294fa76f7a2eee2ef3a40aa924e52ef
Febriyanto Nugroho <febnug@gnuweeb.org>
2022-03-01 08:41:29 +0700

    Add files via upload

---
+++ javascript/eloquent-javascript/program-structure/bindings.js
@@ -0,0 +1,24 @@
+let caught = 5 * 5;
+
+let ten = 10;
+console.log(ten * ten); // 100
+
+
+
+let mood = "light";
+console.log(mood);
+mood = "dark";
+console.log(mood);
+
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt); // 105
+
+let one = 1; 
+let two = 2;
+console.log(one + two); // 3
+
+var name = "Ayda";
+const greeting = "Hello ";
+console.log(greeting + name); // Hello Ayda
