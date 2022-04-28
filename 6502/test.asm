2c149359ab3439e71607cc4f62efbcb583d62141
Febriyanto Nugroho <vmvm1071@gmail.com>
2022-04-28 08:50:17 +0700

    x

---
+++ 6502/test.asm
@@ -0,0 +1,8 @@
+do:                                                                                
+ LDA #$01                                                                   
+ STA $0201 
+ LDA #$05
+ STA $0202
+ LDA #$08
+ STA $0203
+ BNE do
